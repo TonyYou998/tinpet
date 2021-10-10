82795ee367240321b31c54eb442bc88bf8e582df
TonyYou998 <66458772+TonyYou998@users.noreply.github.com>
2021-10-10 22:18:41 +0700

    modify something

--- src/containers/HomeTemplate/HomePage/components/Footer/index.js
+++ src/containers/HomeTemplate/HomePage/components/Footer/index.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Footer() {
     return (
       <div>
-        <footer className="text-center text-lg-start bg-light text-muted">
+        <footer  className="text-center text-lg-start bg-light text-muted">
           {/* Section: Social media */}
           <section className="d-flex justify-content-center justify-content-lg-between p-4 border-bottom">
             
@@ -23,17 +23,13 @@ export default function Footer() {
                   <h6 className="text-uppercase fw-bold mb-4" style={{fontFamily:"Knewave",color:"black"}}>
                      Tinpets
                   </h6>
-                  <p>
-                    Here you can use rows and columns to organize your footer
-                    content. Lorem ipsum dolor sit amet, consectetur adipisicing
-                    elit.
-                  </p>
+                  
                 </div>
                 {/* Grid column */}
                 {/* Grid column */}
                 <div className="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
                   {/* Links */}
-                  <h6 className="text-uppercase fw-bold mb-4">Products</h6>
+                  <h6 style={{color:"black"}} className=" text-uppercase fw-bold mb-4">Products</h6>
                   <p>
                     <a href="#!" className="text-reset">
                       Angular
@@ -59,7 +55,7 @@ export default function Footer() {
                 {/* Grid column */}
                 <div className="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
                   {/* Links */}
-                  <h6 className="text-uppercase fw-bold mb-4">Useful links</h6>
+                  <h6 style={{color:"black"}} className="text-uppercase fw-bold mb-4">Useful links</h6>
                   <p>
                     <a href="#!" className="text-reset">
                       Pricing
@@ -85,7 +81,7 @@ export default function Footer() {
                 {/* Grid column */}
                 <div className="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
                   {/* Links */}
-                  <h6 className="text-uppercase fw-bold mb-4">Contact</h6>
+                  <h6 style={{color:"black"}} className="text-uppercase fw-bold mb-4">Contact</h6>
                   <p>
                     <i className="fas fa-home me-3" /> New York, NY 10012, US
                   </p>
@@ -113,7 +109,7 @@ export default function Footer() {
           >
             © 2021 Copyright:
             <a className="text-reset fw-bold" href="https://mdbootstrap.com/">
-              MDBootstrap.com
+              SUPERTEAM
             </a>
           </div>
           {/* Copyright */}
